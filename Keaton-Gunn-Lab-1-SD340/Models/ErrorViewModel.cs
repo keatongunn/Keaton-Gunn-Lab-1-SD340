84007ea5a2019581e1c19ec007f2e01538200afb
Keaton Gunn <keatongunn@hotmail.com>
2022-03-17 10:07:55 -0500

    Add project files.

---
+++ Keaton-Gunn-Lab-1-SD340/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Keaton_Gunn_Lab_1_SD340.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
