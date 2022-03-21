e8b72f9fac0eab9991dfac9a4fa0b1ff42cb0553
Keaton Gunn <keatongunn@hotmail.com>
2022-03-21 14:43:18 -0500

    Added Room Model

---
+++ Keaton-Gunn-Lab-1-SD340/Models/Room.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Keaton_Gunn_Lab_1_SD340.Models
+{
+    public class Room
+    {
+        [Key]
+        [Display(Name = "Room Number")]
+        public int RoomNumber { get; set; }
+        [Display(Name = "Capacity")]
+        public int Capacity { get; set; }
+
+    }
+    public enum Section
+    {
+        First,
+        Second,
+        Third
+    }
+}
